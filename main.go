afe721875d06668d8c9f43b8cec4291b1bfc7a74
Pedro F. Albanese <68971450+pedroalbanese@users.noreply.github.com>
2022-02-01 12:58:32 -0300

    Update main.go

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func strbig(s string) (i *big.Int) {
 
 var secp128r2 = &elliptic.CurveParams{
 	P: strbig("0xfffffffdffffffffffffffffffffffff"), // Prime
-	N: strbig("0x3fffffff7fffffffbe0024720613b5a3"), // Order
+	N: strbig("0xd6031998d1b3bbfebf59cc9bbff9aee1"), // Order
 	B: strbig("0x5eeefca380d02919dc2c6558bb6d8a5d"), // B
 	Gx: strbig("0x7b6aa5d85e572983e6fb32a7cdebc140"),  // Generator X
 	Gy: strbig("0x27b6916a894d3aee7106fe805fc34b44"),  // Generator Y
