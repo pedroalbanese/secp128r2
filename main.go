980195f312d154a225da7e4e2826cd6f9ff15d6d
Pedro F. Albanese <68971450+pedroalbanese@users.noreply.github.com>
2022-02-01 12:55:08 -0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,23 @@
+package secp128r1
+
+import "crypto/elliptic"
+import "math/big"
+
+func strbig(s string) (i *big.Int) {
+	i = new(big.Int)
+	i.SetString(s,0)
+	return
+}
+
+var secp128r2 = &elliptic.CurveParams{
+	P: strbig("0xfffffffdffffffffffffffffffffffff"), // Prime
+	N: strbig("0x3fffffff7fffffffbe0024720613b5a3"), // Order
+	B: strbig("0x5eeefca380d02919dc2c6558bb6d8a5d"), // B
+	Gx: strbig("0x7b6aa5d85e572983e6fb32a7cdebc140"),  // Generator X
+	Gy: strbig("0x27b6916a894d3aee7106fe805fc34b44"),  // Generator Y
+	BitSize: 128,
+	Name: "secp128r2",
+}
+
+// Secp128r2() returns a Curve which implements Secp128r2
+func Secp128r2() elliptic.Curve { return secp128r2 }
